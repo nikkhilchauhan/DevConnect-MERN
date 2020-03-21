af51e4db5e2fb79597a57651c7a8e2b37827d251
Nikhil Chauhan <nikkhil0018@Nikhils-MacBook-Pro.local>
2020-03-21 21:49:04 +0530

    Fully Initial Setuped

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 
+// Connecting to MongoDB Database
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .then(() => {
+    console.log('MongoDB is Connected');
+  })
+  .catch(error => {
+    console.log(error);
+    // Exit process with failure
+    process.exit(1);
+  });
+
+// Connection configuration
 const PORT = process.env.PORT || 8000;
-
-app.get('/', (req, res) => {
-  res.send("You're genius");
-});
-
 app.listen(PORT, () => {
   console.log('Server running on -> http://localhost:8000/');
+});
+
+// Routes
+app.get('/', (req, res) => {
+  res.send("You're genius");
 });
