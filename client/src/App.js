45afe930272427e1839cb1617797cc5f70ee702d
Nikhil Chauhan <nikkhil0018@Nikhils-MacBook-Pro.local>
2020-04-24 22:48:37 +0530

    client - Protected /Dashboard route

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 import './App.css';
 
 if (localStorage.token) {
@@ -26,6 +28,7 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
